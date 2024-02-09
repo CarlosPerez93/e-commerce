665068a094d34f34661beeab2a12008bf0d04998
CarlosPerez93 <carlos.perez3@outlook.es>
2024-02-09 14:10:07 +0100

    refactor(styles): styles Card Component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,14 +2,24 @@ import product from "./card.type";
 
 import styles from "./Card.module.css";
 
-export const Card = (data: product) => {
+export const Card = ({
+  image,
+  price,
+  title,
+  category,
+  description,
+}: product) => {
   return (
     <div className={styles.main}>
-      <img src={data.image} alt="imagen product" className={styles.img} />
-      <h3 className={styles.h3}>{data.title}</h3>
+      <img src={image} alt="imagen product" className={styles.img} />
+      <h3 className={styles.h3}>{title}</h3>
+      <div className={styles.container}>
+        <p>Category: {category}</p>
+        <p className={styles.description}>Description: {description}</p>
+      </div>
       <p className={styles.p}>
         <strong>price $</strong>
-        {data.price}
+        {price}
       </p>
     </div>
   );
